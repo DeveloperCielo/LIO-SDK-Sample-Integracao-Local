a631f0e3ea6d09cd13e08ff14ab58f0c047c2277
Haroldo Olivieri <luiz.olivieri@m4u.com.br>
2017-03-03 15:36:12 -0300

    action bar flow enhancementa

--- app/src/main/java/cielo/ordermanager/sdk/sample/MainActivity.java
+++ app/src/main/java/cielo/ordermanager/sdk/sample/MainActivity.java
@@ -14,7 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
     }
 
     @OnClick(R.id.parcial_checkout_button)
