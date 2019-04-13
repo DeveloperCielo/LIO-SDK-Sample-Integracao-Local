7a73bff09ecf67c8cbcb5f9ce3ad04389a10b547
Kaique Ocanha <kaique@ocanha.com>
2019-04-13 11:35:48 -0300

    Added permission request for location access and camera access

--- app/src/main/java/com/cielo/ordermanager/sdk/sample/Util.java
+++ app/src/main/java/com/cielo/ordermanager/sdk/sample/Util.java
@@ -1,5 +1,9 @@
 package com.cielo.ordermanager.sdk.sample;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+
 import java.text.NumberFormat;
 import java.util.Locale;
 
@@ -7,5 +11,17 @@ public class Util {
 
     public static String getAmmount(long value) {
         return NumberFormat.getCurrencyInstance(new Locale("pt", "BR")).format((value / 100));
+    }
+
+    public static boolean checkPermissions(String[] permissions, Context context) {
+
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
