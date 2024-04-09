37ae2528967bcda8dac6f01b97db19ead1a874f6
Carlos Vargas <cvargas@braspag.com.br>
2024-04-09 10:40:06 -0300

    add get-payment-products on find order activity

--- app/src/main/java/com/cielo/ordermanager/sdk/sample/FindOrdersActivity.java
+++ app/src/main/java/com/cielo/ordermanager/sdk/sample/FindOrdersActivity.java
@@ -1,13 +1,16 @@
 package com.cielo.ordermanager.sdk.sample;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
@@ -16,18 +19,24 @@ import androidx.appcompat.widget.AppCompatEditText;
 import com.cielo.ordermanager.sdk.R;
 import com.google.gson.GsonBuilder;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cielo.orders.aidl.ParcelableOrder;
 import cielo.orders.domain.Credentials;
 import cielo.orders.domain.Order;
 import cielo.orders.domain.ResultOrders;
+import cielo.orders.domain.product.PrimaryProduct;
 import cielo.sdk.order.OrderManager;
 import cielo.sdk.order.ServiceBindListener;
 import cielo.sdk.order.payment.Payment;
 import timber.log.Timber;
 
+@SuppressLint("LogNotTimber")
 public class FindOrdersActivity extends AppCompatActivity {
+    public final static String TAG = "FindOrdersActivity";
+
     public final static String CLIENT_ID = "rSAqNPGvFPJI";
     private final static String ACCESS_TOKEN = "XZevoUYKmkVr";
 
@@ -102,7 +111,7 @@ public class FindOrdersActivity extends AppCompatActivity {
         orderManager.bind(context, new ServiceBindListener() {
 
             @Override
-            public void onServiceBoundError(Throwable throwable) {
+            public void onServiceBoundError(@NonNull Throwable throwable) {
                 isOrderManagerBinded = false;
                 Timber.tag("bind").w("error");
             }
@@ -128,20 +137,32 @@ public class FindOrdersActivity extends AppCompatActivity {
     }
 
     private void changeFinderFields(RadioGroup rb, int resId) {
-        final boolean showPaymentFields =
-                rb.getCheckedRadioButtonId() == R.id.act_find_orders_rb_findOrderByPayment;
-        clearFields();
-        setLabelRecoverButton(showPaymentFields);
-        setVisibility(R.id.act_find_orders_ll_extraFields1, !showPaymentFields);
-        setVisibility(R.id.act_find_orders_ll_extraFields2, showPaymentFields);
-    }
-
-    private void setLabelRecoverButton(boolean showPaymentFields) {
-        recoverOrderButton.setText(showPaymentFields ? "Recuperar última ordem PAGA" : "Recuperar última ordem");
+        String label;
+        boolean showExtraFields1 = false, showExtraFields2 = false;
+        switch (rb.getCheckedRadioButtonId()) {
+            case R.id.act_find_orders_rb_findOrderById:
+                label = "Recuperar última Ordem";
+                showExtraFields1 = true;
+                break;
+            case R.id.act_find_orders_rb_findOrderByPayment:
+                label = "Recuperar última Ordem PAGA";
+                showExtraFields2 = true;
+                break;
+            case R.id.act_find_orders_rb_findProducts:
+                label = "Recuperar produtos de pagamento";
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + rb.getCheckedRadioButtonId());
+        }
+        clearEditFields();
+        recoverOrderButton.setText(label);
         recoverOrderButton.setVisibility(View.VISIBLE);
+        findOrderButton.setVisibility(View.GONE);
+        setVisibility(R.id.act_find_orders_ll_extraFields1, showExtraFields1);
+        setVisibility(R.id.act_find_orders_ll_extraFields2, showExtraFields2);
     }
 
-    private void clearFields() {
+    private void clearEditFields() {
         amountEditText.setText("");
         authCodeEditText.setText("");
         cieloCodeEditText.setText("");
@@ -155,32 +176,62 @@ public class FindOrdersActivity extends AppCompatActivity {
             return;
         }
         resultOrderEditText.setText("");
-        findOrderButton.setVisibility(View.VISIBLE);
-        resultOrderEditText.setVisibility(View.VISIBLE);
         switch (findMethodRadioGroup.getCheckedRadioButtonId()) {
             case R.id.act_find_orders_rb_findOrderByPayment:
+                findOrderButton.setVisibility(View.VISIBLE);
                 recoverOrderWithPayments();
                 break;
             case R.id.act_find_orders_rb_findOrderById:
+                findOrderButton.setVisibility(View.VISIBLE);
                 recoverAnyOrder();
                 break;
+            case R.id.act_find_orders_rb_findProducts:
+                recoverPaymentTypes();
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + findMethodRadioGroup.getCheckedRadioButtonId());
         }
     }
 
     private void recoverAnyOrder() {
-        final ResultOrders resultOrders = orderManager.retrieveOrders(1, 0);
-        final Order selectedOrder = resultOrders != null && !resultOrders.getResults().isEmpty()
-                ? resultOrders.getResults().get(0)
-                : orderManager.createDraftOrder("Novo Ordem Criada!");
-        if (selectedOrder == null){
+        Order selectedOrder = null;
+        try {
+            final ResultOrders resultOrders = orderManager.retrieveOrders(1, 0);
+            selectedOrder = resultOrders != null && !resultOrders.getResults().isEmpty()
+                    ? resultOrders.getResults().get(0)
+                    : orderManager.createDraftOrder("Nova Ordem Criada!");
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+        if (selectedOrder == null) {
             showMessage("Nenhuma Ordem foi encontrada!");
             return;
         }
         orderIdEditText.setText(selectedOrder.getId());
     }
 
+    private void recoverPaymentTypes() {
+        List<PrimaryProduct> primaryProductList = null;
+        try {
+            primaryProductList = orderManager.retrievePaymentType(this.getApplicationContext());
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+        if (primaryProductList == null) {
+            showMessage("Nenhum Produto foi encontrado!");
+            return;
+        }
+        resultOrderEditText.setText(toJson(primaryProductList));
+        showMessage("O Produtos foram obtidos com sucesso!");
+    }
+
     private void recoverOrderWithPayments() {
-        final ResultOrders resultOrders = orderManager.retrieveOrders(1, 0);
+        ResultOrders resultOrders = null;
+        try {
+            resultOrders = orderManager.retrieveOrders(1, 0);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
         if (resultOrders == null || resultOrders.getResults().isEmpty()) {
             showMessage("Nenhuma Ordem foi encontrada!");
             return;
@@ -211,6 +262,7 @@ public class FindOrdersActivity extends AppCompatActivity {
             return;
         }
         resultOrderEditText.setText("");
+        resultOrderEditText.setVisibility(View.VISIBLE);
         switch (findMethodRadioGroup.getCheckedRadioButtonId()) {
             case R.id.act_find_orders_rb_findOrderByPayment:
                 findOrderByPayment();
@@ -218,12 +270,19 @@ public class FindOrdersActivity extends AppCompatActivity {
             case R.id.act_find_orders_rb_findOrderById:
                 findOrderById();
                 break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + findMethodRadioGroup.getCheckedRadioButtonId());
         }
     }
 
     private void findOrderById() {
         final String orderId = getStringValue(orderIdEditText);
-        final ParcelableOrder order = orderManager.retrieveOrderById(orderId);
+        ParcelableOrder order = null;
+        try {
+            order = orderManager.retrieveOrderById(orderId);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
         if (order == null) {
             showMessage("A ordem não foi localizada!");
             return;
@@ -232,26 +291,32 @@ public class FindOrdersActivity extends AppCompatActivity {
         showMessage("A ordem foi localizada atráves do Id!");
     }
 
+    @SuppressLint("LogNotTimber")
     private void findOrderByPayment() {
         final long amount = getStringValue(amountEditText).isEmpty() ?
                 0L : Long.parseLong(getStringValue(amountEditText));
         final String authCode = getStringValue(authCodeEditText);
         final String cieloCode = getStringValue(cieloCodeEditText);
-        final ParcelableOrder order = orderManager.findOrder(amount, authCode, cieloCode);
+        ParcelableOrder order = null;
+        try {
+            order = orderManager.findOrder(amount, authCode, cieloCode);
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
         if (order == null) {
             showMessage("A ordem não foi localizada!!!");
             return;
         }
-        showMessage("A ordem foi localizada com base nos dados de pagamento!!!");
         resultOrderEditText.setText(toJson(order));
+        showMessage("A ordem foi localizada com base nos dados de pagamento!!!");
     }
 
     private void showMessage(final String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 
-    private String getStringValue(final EditText edit){
-        return edit != null && edit.getText() != null? edit.getText().toString() : "";
+    private String getStringValue(final EditText edit) {
+        return edit != null && edit.getText() != null ? edit.getText().toString() : "";
     }
 
     private void setVisibility(int resId, boolean isVisible) {
